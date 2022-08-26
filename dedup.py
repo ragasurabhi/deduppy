796b460452158bb472e86f68835a817d76083aa6
Sriram V <sriram_varadarajan@apple.com>
2022-08-26 12:36:01 +0800

    support dup positions

--- dedup.py
+++ dedup.py
@@ -9,23 +9,25 @@ def process_bag(mybag):
 
   # simple strategy to pick winner - pick first
   winner = mybag[0]
-  print(winner)
+  dup_positions = [x[0] for x in mybag[1:]]
+  print(f"{','.join(winner[1:])}|{','.join(dup_positions)}|")
 
 def main():
   the_bag = []
-  patt = re.compile(r"^(?P<rownum>\w+),(?P<rowkey>\w+),")
+  patt = re.compile(r"^(?P<col_one>.+?),(?P<col_two>.+?),(?P<col_rest>.+)")
   (prev, curr) = (None, None)
   
   for line in fileinput.input():
     m = patt.match(line)
-    curr = m.group("rowkey")
+    curr = m.group("col_rest")
     #print(f"curr={curr}, prev={prev}, bagsize={len(the_bag)}")
     if curr != prev:
       process_bag(the_bag)
       prev = curr
       the_bag = []
-    the_bag.append(line.strip())
+    the_bag.append((m.group("col_one"), m.group("col_two"), m.group("col_rest")))
   process_bag(the_bag)
 
 main()
+
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -x
 
-cat input.txt | sort -k2 -t\, > junk1.del
-cat junk1.del | ./dedup.py  > output.txt
+cat input.txt | perl -ne 'chomp; print "$.,$_\n";' | sort -k3 -t\, | tee junk1.del
+cat junk1.del | ./dedup.py  | tee output.txt
 
